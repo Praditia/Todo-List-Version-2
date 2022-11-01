ef18d391ca79b6386f6a5a23fb71fd4410b1c0c3
aditfauzi <praditia66@yahoo.com>
2022-11-01 12:50:20 +0700

    Finish

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const _ = require('lodash')
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/todolistDB');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('public'));
 
@@ -51,41 +54,77 @@ app.get('/', function (req, res) {
       });
       res.redirect('/');
     } else {
-      res.render('list', { listTitle: 'Today', newListItems: foundItems });
+      res.render('list', {
+        listTitle: 'Today',
+        newListItems: foundItems
+      });
     }
   });
 });
 
 app.get('/:customeListName', function (req, res) {
-  const customeListName = req.params.customeListName;
+  const customeListName = _.capitalize(req.params.customeListName)
 
-  const list = new List({
-    name: customeListName,
-    items: defaultItems,
-  });
-
-  list.save();
+  List.findOne({
+    name: customeListName
+  }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customeListName,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect('/' + customeListName)
+      } else {
+        res.render('list', {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        })
+      }
+    }
+  })
 });
 
 app.post('/', function (req, res) {
   const itemName = req.body.newItem;
+  const listName = req.body.list
   const item = new Item({
     name: itemName,
   });
 
-  item.save();
+  if (listName === 'Today') {
+    item.save();
+    res.redirect('/');
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item)
+      foundList.save()
+      res.redirect('/' + listName)
+    })
+  }
 
-  res.redirect('/');
 });
 
 app.post('/delete', function (req, res) {
   const checkedItemId = req.body.checkbox;
-  Item.findByIdAndRemove(checkedItemId, function (err) {
-    if (!err) {
-      console.log('Deleted successfully');
-      res.redirect('/');
-    }
-  });
+  const listName = req.body.listName
+
+  if (listName === 'Today') {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log('Deleted successfully');
+        res.redirect('/');
+      }
+    });
+  } else {
+    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
+      if (!err) {
+        res.redirect('/' + listName)
+      }
+    })
+  }
+
 });
 
 app.listen(3000, function () {
